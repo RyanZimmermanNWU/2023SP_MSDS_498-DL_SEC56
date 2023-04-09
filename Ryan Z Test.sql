861fcedf5d483c5d754d891020905ea7da72447e
RyanZimmermanNWU <130259527+RyanZimmermanNWU@users.noreply.github.com>
2023-04-08 18:16:10 -0700

    Add files via upload
    Signed-off-by: RyanZimmermanNWU <130259527+RyanZimmermanNWU@users.noreply.github.com>

---
+++ Ryan Z Test.sql
@@ -0,0 +1,8 @@
+-- Databricks notebook source
+-- DBTITLE 1,Getting Started
+-- MAGIC %sh git clone https://github.com/RyanZimmermanNWU/2023SP_MSDS_498-DL_SEC56.git /dbfs/FileStore/RADS/
+
+-- COMMAND ----------
+
+-- DBTITLE 1,Run to refresh your data
+-- MAGIC %sh git -C /dbfs/FileStore/RADS/ pull
