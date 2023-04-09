dd72c0d0b62ad1fc565c329bbed00ea03a6caf9f
RyanZimmermanNWU <130259527+RyanZimmermanNWU@users.noreply.github.com>
2023-04-08 23:55:05 -0700

    Add files via upload
    Signed-off-by: RyanZimmermanNWU <130259527+RyanZimmermanNWU@users.noreply.github.com>

--- Ryan Z Test.sql
+++ Ryan Z Test.sql
@@ -1,8 +1,14 @@
 -- Databricks notebook source
 -- DBTITLE 1,Getting Started
--- MAGIC %sh git clone https://github.com/RyanZimmermanNWU/2023SP_MSDS_498-DL_SEC56.git /dbfs/FileStore/RADS/
+-- MAGIC %sh git clone https://github.com/RyanZimmermanNWU/2023SP_MSDS_498-DL_SEC56.git --depth 1 --branch=main /dbfs/FileStore/RADS1/
 
 -- COMMAND ----------
 
 -- DBTITLE 1,Run to refresh your data
--- MAGIC %sh git -C /dbfs/FileStore/RADS/ pull
+-- MAGIC %sh git -C /dbfs/FileStore/RADS1/ pull
+
+-- COMMAND ----------
+
+-- DBTITLE 1,Export your notebook
+#https://stackoverflow.com/questions/53169360/databricks-github-integration-automatically-add-all-notebooks-to-repository
+#https://www.databricks.com/blog/2017/11/08/introducing-command-line-interface-for-databricks-developers.html
